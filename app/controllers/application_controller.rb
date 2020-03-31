13f5c7698c7e0b9b8ef1b43de0a7d6427c57c2fe
Rooted1 <ruthobey@gmail.com>
2020-03-31 11:41:25 -0500

    create controllers to display views on browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 class ApplicationController < Sinatra::Base
 
     set(:views, 'app/views')
-    
+# get method to display all airlines
+    get '/list_airlines' do
+      erb(:list_airlines)
+    end
 
+# get method to display a single airline by id
+    get '/get_airline/:id' do
+      @airline = Airline.find_by({id: params[:id]})
+      erb(:single_airline)
+    end
+
+# delete method to delete an airline by id 
+    delete '/get_airline/:id' do
+      @airline = Airline.find_by({id: params[:id]})
+      @airline.destroy()
+
+      redirect ("/list_airlines")
+    end
 end
 
 
